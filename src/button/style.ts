47ac93d9ed04516eed954c760f7c03928da115b0
WMXPY <wm@mengw.io>
2019-01-24 09:59:04 -0600

    fix button full size

--- src/button/style.ts
+++ src/button/style.ts
@@ -51,8 +51,8 @@ const NeonButtonStyleBase: JSSStyle = {
     },
     sizeFullBox: {
         boxSizing: 'border-box',
-        width: 'inherit',
-        height: 'inherit',
+        width: '100%',
+        height: '100%',
     },
     sizeFull: {
         width: '100%',
