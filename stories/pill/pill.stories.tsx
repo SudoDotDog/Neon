31272ec01a5d8f2c1d0cc76e929850fbd269880b
WMXPY <wm@mengw.io>
2019-02-04 20:27:23 +0800

    implement pill story

---
+++ stories/pill/pill.stories.tsx
@@ -0,0 +1,25 @@
+/**
+ * @author WMXPY
+ * @namespace Stories_Pill
+ * @description Pill
+ */
+
+import { text } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import { NeonPill } from "../../src/pill";
+import { NeonPillBase } from "../../src/pill/pill";
+import { wInfo } from "../util";
+import PillMDX from "./pill.mdx";
+
+const componentStories = storiesOf('Components', module);
+
+componentStories.add(
+    'Pill',
+    wInfo([NeonPillBase], [NeonPill, PillMDX])(() => {
+
+        const info: string = text('Info', 'info');
+
+        return (<PillMDX info={info} />);
+    }),
+);
