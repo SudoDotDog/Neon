fd6afeffefd8f518baff3d2de233772461034d8a
WMXPY <wm@mengw.io>
2019-02-04 20:57:06 +0800

    improve pill style

--- src/pill/style.ts
+++ src/pill/style.ts
@@ -11,17 +11,20 @@ import { getBorder, JSSStyle } from "../#common/style";
 const NeonPillStyleBase: JSSStyle = {
 
     wrap: {
+
         display: 'inline-flex',
         alignItems: 'center',
         height: '1.5rem',
         ...getBorder(COLOR.NAVY, '0.1rem'),
     },
     info: {
+
         paddingLeft: '0.3rem',
         paddingRight: '0.3rem',
         flex: 1,
     },
     remove: {
+
         width: '1.5rem',
     },
 };
