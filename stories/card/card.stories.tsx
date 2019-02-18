62ffbc8d4bb62006a1a890e654a820ff86495453
WMXPY <wm@mengw.io>
2019-02-18 16:25:37 -0600

    merge card component

---
+++ stories/card/card.stories.tsx
@@ -0,0 +1,25 @@
+/**
+ * @author WMXPY
+ * @namespace Stories_Components
+ * @description Card
+ */
+
+import { text } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import { NeonCard } from "../../src/card";
+import { NeonCardBase } from "../../src/card/card";
+import { wInfo } from "../util";
+import CardMDX from "./card.mdx";
+
+const componentStories = storiesOf('Card', module);
+
+componentStories.add(
+    'Card',
+    wInfo([NeonCardBase], [NeonCard, CardMDX])(() => {
+
+        const name: string = text('Name', 'Name');
+
+        return (<CardMDX name={name} />);
+    }),
+);
