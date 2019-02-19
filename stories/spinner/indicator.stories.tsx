efb72352244a57a4fb18c5ab7af6a077543c69f7
WMXPY <wm@mengw.io>
2019-02-19 17:00:23 -0600

    integrate indicator story

--- stories/spinner/indicator.stories.tsx
+++ stories/spinner/indicator.stories.tsx
@@ -8,43 +8,32 @@ import { boolean, text } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import { NeonButton } from "../../src/button";
-import { SIZE, WIDTH } from "../../src/declare";
 import { NeonInput } from "../../src/input";
 import { NeonPaper } from "../../src/paper";
 import { NeonIndicator } from "../../src/spinner";
 import { NeonIndicatorBase } from "../../src/spinner/indicator";
 import { NeonSpinnerBase } from "../../src/spinner/spinner";
 import { wInfo } from "../util";
+import IndicatorMDX from "./indicator.mdx";
 
-const componentStories = storiesOf('Scenario', module);
+const componentStories = storiesOf('Spinner', module);
 
 componentStories.add(
     'Indicator',
     wInfo(
         [NeonSpinnerBase, NeonIndicatorBase],
-        [NeonButton, NeonInput, NeonPaper, NeonIndicator],
+        [NeonButton, NeonInput, NeonPaper, NeonIndicator, IndicatorMDX],
     )(() => {
 
         const label: string = text('Label', 'Label');
         const loading: boolean = boolean('Loading', false);
         const covering: boolean = boolean('Covering', false);
 
-        return (<div>
-            <NeonPaper>
-                From
-                <NeonIndicator loading={loading} covering={covering} cover={label}>
-                    <div>
-                        <NeonInput label={label} />
-                        <NeonInput label={label} />
-                        <NeonInput label={label} />
-                        <NeonButton width={WIDTH.FULL} size={SIZE.LARGE}>Hello</NeonButton>
-                    </div>
-                </NeonIndicator>
-                <hr />
-                Out of indicator
-                <NeonButton>Hello</NeonButton>
-            </NeonPaper>
-        </div>);
+        return (<IndicatorMDX
+            label={label}
+            loading={loading}
+            covering={covering}
+        />);
     }),
 );
 
