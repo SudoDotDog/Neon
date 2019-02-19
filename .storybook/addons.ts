29a7102bc1120b8a982b1cd12d8f2c000d6c825d
WMXPY <wm@mengw.io>
2019-02-19 16:25:37 -0600

    consume ally

--- .storybook/addons.ts
+++ .storybook/addons.ts
@@ -4,6 +4,8 @@
  * @description Addon
  */
 
+import '@storybook/addon-a11y/register';
 import '@storybook/addon-actions/register';
 import "@storybook/addon-knobs/register";
 import '@storybook/addon-storysource/register';
+
--- .storybook/config.ts
+++ .storybook/config.ts
@@ -4,10 +4,12 @@
  * @description Config
  */
 
+import { checkA11y } from '@storybook/addon-a11y';
 import { withKnobs } from "@storybook/addon-knobs/react";
 import { addDecorator, configure } from "@storybook/react";
 
 addDecorator(withKnobs);
+addDecorator(checkA11y);
 
 const req: __WebpackModuleApi.RequireContext = require.context('../stories', true, /\.stories\.tsx$/);
 
