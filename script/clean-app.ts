ef30e4dd57edf2de98125cc08c717d17f3b30548
WMXPY <wm@mengw.io>
2019-03-06 16:59:09 -0600

    2.9.0 release add grid, bug fix

--- script/clean-app.ts
+++ script/clean-app.ts
@@ -10,8 +10,13 @@ import { rmRFFolderSync } from './util';
 
 const appPath: string = Path.join(__dirname, '..', 'app');
 
+if (!Fs.existsSync(appPath)) {
+    Fs.mkdirSync(appPath);
+}
+
 const files: string[] = Fs.readdirSync(appPath);
 for (const file of files) {
 
     rmRFFolderSync(Path.join(appPath, file));
 }
+
