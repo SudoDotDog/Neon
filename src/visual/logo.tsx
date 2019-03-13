491e7ee46bb9889a694f8b75132d95c1071aa2f6
WMXPY <wm@mengw.io>
2019-03-13 16:28:58 -0500

    implement logo component

--- src/visual/logo.tsx
+++ src/visual/logo.tsx
@@ -9,12 +9,18 @@ import * as React from "react";
 import { boxProps, NeonBox } from "../#common/components/box";
 import { ThemedComponent, ThemeProps, withConsumer } from "../#common/consumer";
 import { BoxProps } from "../#common/declare";
+import { mergeClasses } from "../#common/style/decorator";
+import { NeonSquare } from "./square";
 import { NeonLogoStyle } from "./style/logo";
 
 export type NeonLogoProps = {
 
+    readonly width?: string;
+
     readonly title: string;
+    readonly titleFontSize?: string;
     readonly sub?: string;
+    readonly subFontSize?: string;
 } & ThemeProps & BoxProps;
 
 export const NeonLogoBase: React.FC<NeonLogoProps> = (props: NeonLogoProps) => {
@@ -22,10 +28,35 @@ export const NeonLogoBase: React.FC<NeonLogoProps> = (props: NeonLogoProps) => {
     const logoStyle: Classes = NeonLogoStyle.use();
 
     return <NeonBox
-        {...boxProps(props)}
+        {...boxProps(
+            props,
+            logoStyle.wrap,
+        )}
     >
-        <div className={logoStyle.title}>{props.title}</div>
-        <div className={logoStyle.sub}>{props.sub}</div>
+        <NeonSquare
+            width={props.width || "8rem"}
+            className={logoStyle.title}
+            style={{
+                fontSize: props.titleFontSize || '3rem',
+            }}
+        >
+            <div className={mergeClasses(
+                logoStyle.alignBottom,
+                logoStyle.paddingMore,
+            )}>{props.title}</div>
+        </NeonSquare>
+        <div
+            className={logoStyle.sub}
+            style={{
+                fontSize: props.subFontSize || '2rem',
+            }}
+        >
+            <div className={logoStyle.placeholder}>{props.sub}</div>
+            <div className={mergeClasses(
+                logoStyle.alignBottom,
+                logoStyle.innerSub,
+            )}>{props.sub}</div>
+        </div>
     </NeonBox>;
 };
 
--- src/visual/style/logo.ts
+++ src/visual/style/logo.ts
@@ -4,19 +4,39 @@
  * @description Logo
  */
 
+import { COLOR } from "../../#common/declare";
 import { JSSStyle } from "../../#common/style/decorator";
 import { NeonStyle } from "../../#common/style/style";
 
 const NeonLogoStyleBase: JSSStyle = {
 
-    dark: {
-
+    wrap: {
+        display: 'flex',
     },
     title: {
-        fontSize: '3rem',
+        backgroundColor: COLOR.NAVY,
+        boxSizing: 'border-box',
+        color: COLOR.WHITE,
     },
     sub: {
+        color: COLOR.NAVY,
+        position: 'relative',
+        border: `5px solid ${COLOR.NAVY}`,
+        boxSizing: 'border-box',
         textAlign: 'justify',
+    },
+    placeholder: {
+        visibility: 'hidden',
+        padding: '0.3rem',
+    },
+    alignBottom: {
+        position: 'absolute',
+        padding: '0.3rem',
+        right: 0,
+        bottom: 0,
+    },
+    paddingMore: {
+        paddingBottom: '0.6rem',
     },
 };
 
